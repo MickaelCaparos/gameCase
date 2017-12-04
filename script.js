b1973921ff3eaaba862ece5fa6539773ddf5c108
MickaelCaparos <33620330+MickaelCaparos@users.noreply.github.com>
2017-12-04 14:12:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var squareNb = "";
+var secNb = "";
+var roundsNb = "";
+
+function squareGame__start() {
+ // On récupère les variables
+ squareNb = document.querySelector("#squareNb").value;
+ secNb = document.querySelector("#secNb").value;
+ roundsNb = document.querySelector("#roundsNb").value;
+}
